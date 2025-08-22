3666c598158b0957fe47f40df49bdc1ae30b6895
pippinbarr <pippin.barr@gmail.com>
2025-08-22 09:21:54 -0400

    Added variables for fly and frog, move frog on x only
    - By setting only the frog's x to the mouse I can lock it to the bottom
    - Currently the fly doesn't move, I guess that's next
    - I should also start breaking this into functions soon

--- js/script.js
+++ js/script.js
@@ -7,12 +7,35 @@
 
 "use strict";
 
+const frog = {
+    // Will set position in setup based on canvas size
+    x: undefined,
+    y: undefined,
+    // Size is the diameter of the circle
+    size: 140
+};
+
+const fly = {
+    // Position (this will need to change for the fly to move)
+    x: 0,
+    y: 200,
+    // Size is the diameter of the circle
+    size: 15,
+    // Speed the fly moves left to right
+    speed: 5
+};
+
 /**
  * Create a canvas to draw on
 */
 function setup() {
     // Create a 640x480 canvas
     createCanvas(640, 480);
+
+    // Position the frog in the bottom center
+    // We will set its x based on the mouse every frame
+    frog.x = width / 2;
+    frog.y = height;
 }
 
 /**
@@ -22,18 +45,20 @@ function draw() {
     // Fill the background blue
     background(110, 150, 250);
 
+    // Update the frog's position to the mouse's x
+    frog.x = mouseX;
+
     // Draw the fly somewhere just to see it for now
     push();
     fill(0, 0, 0);
     noStroke();
-    ellipse(200, 200, 15, 15);
+    ellipse(fly.x, fly.y, fly.size);
     pop();
 
-    // Draw the frog at the mouse position
-    // (I'll need to make a frog variable for this in a sec)
+    // Draw the frog at its position
     push();
     fill(110, 250, 140);
     noStroke();
-    ellipse(mouseX, mouseY, 140, 140);
+    ellipse(frog.x, frog.y, frog.size);
     pop();
 }

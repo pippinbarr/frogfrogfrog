e99f52f08ca263bbb1090885a25e9a73af9d8e6c
pippinbarr <pippin.barr@gmail.com>
2025-08-22 09:16:34 -0400

    Drawing a frog and a fly
    - Right now it's just to see something on the canvas
    - Obviously the plan is not for the frog's whole head to move so I'll need to lock it to the bottom of the screen and work on the tongue
    - Need variables for the fly and the frog

--- js/script.js
+++ js/script.js
@@ -2,23 +2,38 @@
  * FrogFrogFrog
  * Pippin Barr
  * 
- * This will be a simple frog-catching-flies game eventually
- * But for now it's just the skeleton of my code
+ * In progress game about catching flies as a frog.
  */
 
 "use strict";
 
 /**
- * Remember to describe setup here
+ * Create a canvas to draw on
 */
 function setup() {
-
+    // Create a 640x480 canvas
+    createCanvas(640, 480);
 }
 
-
 /**
- * Remember to describe draw here
+ * Draw the frog where the mouse is and draw a fly
 */
 function draw() {
+    // Fill the background blue
+    background(110, 150, 250);
 
+    // Draw the fly somewhere just to see it for now
+    push();
+    fill(0, 0, 0);
+    noStroke();
+    ellipse(200, 200, 15, 15);
+    pop();
+
+    // Draw the frog at the mouse position
+    // (I'll need to make a frog variable for this in a sec)
+    push();
+    fill(110, 250, 140);
+    noStroke();
+    ellipse(mouseX, mouseY, 140, 140);
+    pop();
 }
